2c507695cb874514024d37bb1c9255ab103815b1
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-05-17 16:44:06 -0600

    Apostrophe fix

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
     return (
         <div className="bg-gray-600 ">
-            <p>Chloe's WebDev 2 Website</p>
+            <p>Chloe&apos;s WebDev 2 Website</p>
             <Link href="/">
                 <span className="text-blue-500 hover:underline">Home</span>
             </Link>
