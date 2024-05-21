029adf90a01399d426a35e52fec828962ed63e1d
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-05-21 11:51:00 -0600

    Add week 3 page

--- app/page.js
+++ app/page.js
@@ -8,7 +8,10 @@ export default function Home() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">
         <span className="text-blue-500 hover:underline">Week 2</span>
-        
+      </Link>
+
+      <Link href="/week-3">
+        <span className="text-blue-500 hover:underline">Week 3</span>
       </Link>
     </div>
 
