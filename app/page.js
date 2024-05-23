a01b208cb848477535e57f1df6eb560d14f6eacd
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-05-23 12:57:20 -0600

    Header Styling

--- app/page.js
+++ app/page.js
@@ -3,17 +3,19 @@ import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">
-        <span className="text-blue-500 hover:underline">Week 2</span>
-      </Link>
-      <br/>
-      <Link href="/week-3">
-        <span className="text-blue-500 hover:underline">Week 3</span>
-      </Link>
-    </div>
+    <main>
+      <div>
+        <Header/>
+          <h1>CPRG 306: Web Development 2 - Assignments</h1>
+          <Link href="/week-2">
+            <span className="text-blue-500 hover:underline">Week 2</span>
+          </Link>
+          <br/>
+          <Link href="/week-3">
+            <span className="text-blue-500 hover:underline">Week 3</span>
+          </Link>
+      </div>
+    </main>
 
   );
 }
--- components/header.js
+++ components/header.js
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <div className="bg-gray-600 ">
-            <p>Chloe&apos;s WebDev 2 Website</p>
+        <div className="bg-gray-600 flex items-center space-x-10 rounded mt-2">
+            <p className="m-2 text-2xl">
+                Chloe&apos;s WebDev 2 Website</p>
             <Link href="/">
-                <span className="text-blue-500 hover:underline">Home</span>
+                <button className="text-blue-500 hover:underline bg-red-100 rounded p-1">
+                    Home</button>
             </Link>
             
         </div>
