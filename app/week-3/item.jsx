ec442ecb843119a735997a8d902c8b130fe9f9a3
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-05-27 09:53:54 -0600

    bug fix

--- app/week-3/item.jsx
+++ app/week-3/item.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ItemsList from "./item-list";
 
 const Item =({List})=>{
     return <div className="flex flex-wrap justify-center">
         {List && List.map((I) =>(
-        <div className="bg-gradient-to-br from-blue-700 to-yellow-600 m-1 p-2 w-80">
+        <div className="bg-gradient-to-br from-blue-700 to-yellow-600 m-1 p-2 w-80 rounded-lg">
+            
             <div className="text-xl font-semibold">{I.name}</div>
             Amount: {I.quantity} | Department: {I.category}
         </div>))}
