f4c75ffe6f3ac1de729e63e17a33b42fe5c53842
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-05-27 10:03:26 -0600

    hopefully bug fixed!

--- app/week-3/item.jsx
+++ app/week-3/item.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Item =({List})=>{
     return <div className="flex flex-wrap justify-center">
-        {List && List.map((I) =>(
-        <div className="bg-gradient-to-br from-blue-700 to-yellow-600 m-1 p-2 w-80 rounded-lg">
+        {List && List.map((I, index) =>(
+        <div key={index} // using index as a unique key because the items don't have unique IDs
+        className="bg-gradient-to-br from-blue-700 to-yellow-600 m-1 p-2 w-80 rounded-lg">
             
             <div className="text-xl font-semibold">{I.name}</div>
             Amount: {I.quantity} | Department: {I.category}
