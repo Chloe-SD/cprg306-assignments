8547976e1905595479b9c266166f816f49a38d8b
Chloe-SD <chloe.nibali@edu.sait.ca>
2024-06-04 19:49:02 -0600

    form submit functionality

--- app/week-4/new-item.jsx
+++ app/week-4/new-item.jsx
@@ -7,20 +7,43 @@ const NewItem = () => {
     const [quantity, setQuantity] = useState(1);
     const [category, setCategory] = useState("produce");
 
+    const handleSubmit = (event) => {
+        event.preventDefault(); // prevents default form submission behavior
+        const item = { name, quantity, category }; // create an item to hold the form inputs
+        console.log(item); // write the item to console
+        alert(`You have added the Item:\nName: ${name}\nQuantity: ${quantity}\nCategory: ${category}`);
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+    };
+
     return <div>
-        <form className='flex-col flex items-center p-4
+        <form onSubmit={handleSubmit} 
+        className='flex-col flex items-center p-4
         bg-gradient-to-br from-blue-700 to-yellow-600 rounded-lg'>
+            
             <div className='flex justify-center'>
                 <p className='flex items-center px-5'>Item Name:</p>
-                <input type="text" placeholder="Item Name" required/>
+                <input type="text" 
+                value={name}
+                onChange={(e) => setName(e.target.value)} 
+                placeholder="Item Name" 
+                required/>
             </div>
+            
             <div className='flex justify-center'>
                 <p className='flex items-center px-5'>Quantity:</p>
-                <input type='number' min="1" max="99" required></input>
+                <input type='number'
+                value={quantity} 
+                onChange={(e) => setQuantity(Number(e.target.value))}
+                min="1" max="99" 
+                required/>
             </div>
+            
             <div className='flex justify-center'>
                 <p className='flex items-center justify-end px-5'>Category:</p>
-                <select>
+                <select value={category}
+                onChange={(e) => setCategory(e.target.value)}>
                     <option value disabled="Category"></option>
                     <option value="produce" selected>Produce</option>
                     <option value="dairy">Dairy</option>
